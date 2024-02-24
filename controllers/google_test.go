01b1daf31ef61ff46ac18b4b8d6c49f1f7ab5ea3
Armaan Katyal <armaankatyal3@gmail.com>
2024-02-23 23:07:26 -0700

    feat: added more tests

--- controllers/google_test.go
+++ controllers/google_test.go
@@ -1,9 +1,14 @@
 package controllers
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/ArmaanKatyal/go-oauth2/internal"
+	"github.com/alicebob/miniredis/v2"
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -23,5 +28,29 @@ func TestGenerateState(t *testing.T) {
 }
 
 func TestGenerateJWT(t *testing.T) {
+	user := map[string]interface{}{
+		"email":          "test",
+		"name":           "test",
+		"verified_email": true,
+	}
+	token, err := generateJWT(user)
+	if assert.NoError(t, err) {
+		assert.NotEmpty(t, token)
+	}
+}
 
+func TestGoogleLogin(t *testing.T) {
+	s := miniredis.RunT(t)
+	defer s.Close()
+
+	internal.InitializeRedis(s.Host(), s.Port())
+
+	req := httptest.NewRequest(http.MethodGet, "/google_login", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := echo.New().NewContext(req, rec)
+	if assert.NoError(t, GoogleLogin(ctx)) {
+		assert.Equal(t, http.StatusFound, rec.Code)
+		assert.NotEmpty(t, rec.Header().Get("Location"))
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ArmaanKatyal/go-oauth2
 go 1.21.7
 
 require (
+	github.com/alicebob/miniredis/v2 v2.31.1
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.11.4
 	github.com/stretchr/testify v1.8.4
@@ -10,10 +11,12 @@ require (
 )
 
 require (
+	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/yuin/gopher-lua v1.1.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
